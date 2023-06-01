f981a0019f15a253be6e75b7f2d9d4428a297417
cariks <eriks.cariks.lvt@gmail.com>
2023-06-01 15:16:22 +0300

    Sakums

---
+++ Assets/Skripti/NomesanasVieta.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class NomesanasVieta : MonoBehaviour, IDropHandler {
+	private float vietasZRot, velkObjZRot, rotacijasStarpiba;
+	private Vector2 vietasIzm, velkObjIzm;
+	private float xIzmeruStarp, yIzmeruStarp;
+	public Objekti objektuSkipts;
+
+
+    public void OnDrop(PointerEventData eventData)
+    {
+       if(eventData.pointerDrag != null)
+		{
+			if(eventData.pointerDrag.tag.Equals(tag))
+			{
+                velkObjZRot =
+				eventData.pointerDrag.GetComponent<RectTransform>()
+				.transform.eulerAngles.z;
+
+                vietasZRot = 
+				GetComponent<RectTransform>().transform.eulerAngles.z;
+
+				rotacijasStarpiba = Mathf.Abs(vietasZRot - velkObjZRot);
+
+                velkObjIzm =
+				eventData.pointerDrag.GetComponent<RectTransform>().localScale;
+
+                vietasIzm = GetComponent<RectTransform>().localScale;
+
+				xIzmeruStarp = Mathf.Abs(vietasIzm.x - velkObjIzm.x);
+				yIzmeruStarp = Mathf.Abs(vietasIzm.y - velkObjIzm.y);
+				
+				Debug.Log("Rotācijas starpība: "+rotacijasStarpiba
+					+"\nx starpība: "+xIzmeruStarp
+					+"\ny starpība: "+yIzmeruStarp);
+
+				if((rotacijasStarpiba <=6 || 
+					(rotacijasStarpiba >= 354 && 
+					rotacijasStarpiba <= 360)) &&
+					(xIzmeruStarp <= 0.1 && yIzmeruStarp <= 0.1))
+				{
+					Debug.Log("Nolikts pareizajā vietā!");
+					objektuSkipts.vaiIstajaVieta = true;
+					
+					eventData.pointerDrag.
+						GetComponent<RectTransform>().anchoredPosition =
+						GetComponent<RectTransform>().anchoredPosition;
+
+					eventData.pointerDrag.
+						GetComponent<RectTransform>().localRotation =
+						GetComponent<RectTransform>().localRotation;
+
+					eventData.pointerDrag.
+						GetComponent<RectTransform>().localScale = 
+						GetComponent<RectTransform>().localScale;
+
+
+					switch(eventData.pointerDrag.tag){
+						case "atkritumi":
+							objektuSkipts.skanasAvots.
+							PlayOneShot(objektuSkipts.skanaKoAtskanot[1]);
+							break;
+
+						case "medicina":
+                            objektuSkipts.skanasAvots.
+                            PlayOneShot(objektuSkipts.skanaKoAtskanot[2]);
+                            break;
+
+						case "buss":
+                            objektuSkipts.skanasAvots.
+                            PlayOneShot(objektuSkipts.skanaKoAtskanot[3]);
+                            break;
+					case "cement":
+						objektuSkipts.skanasAvots.
+						PlayOneShot(objektuSkipts.skanaKoAtskanot[4]);
+						break;
+					case "e46":
+						objektuSkipts.skanasAvots.
+						PlayOneShot(objektuSkipts.skanaKoAtskanot[5]);
+						break;
+					case "b2":
+						objektuSkipts.skanasAvots.
+						PlayOneShot(objektuSkipts.skanaKoAtskanot[6]);
+						break;
+					case "policija":
+						objektuSkipts.skanasAvots.
+						PlayOneShot(objektuSkipts.skanaKoAtskanot[7]);
+						break;
+					case "eskavators":
+						objektuSkipts.skanasAvots.
+						PlayOneShot(objektuSkipts.skanaKoAtskanot[8]);
+						break;
+					case "traktors1":
+						objektuSkipts.skanasAvots.
+						PlayOneShot(objektuSkipts.skanaKoAtskanot[9]);
+						break;
+					case "traktors5":
+						objektuSkipts.skanasAvots.
+						PlayOneShot(objektuSkipts.skanaKoAtskanot[10]);
+						break;
+					case "e61":
+						objektuSkipts.skanasAvots.
+						PlayOneShot(objektuSkipts.skanaKoAtskanot[11]);
+						break;
+					case "ugunsdzeseji":
+						objektuSkipts.skanasAvots.
+						PlayOneShot(objektuSkipts.skanaKoAtskanot[12]);
+						break;
+
+
+						default:
+							Debug.Log("Tags netika atpazīts!");
+							break;
+					}
+				}
+
+			} else {
+
+				objektuSkipts.vaiIstajaVieta = false;
+				objektuSkipts.skanasAvots.
+					PlayOneShot(objektuSkipts.skanaKoAtskanot[0]);
+
+                switch (eventData.pointerDrag.tag){
+                    case "atkritumi":
+						objektuSkipts.atkritumuMasina.
+							 GetComponent<RectTransform>().localPosition =
+							 objektuSkipts.atkrMKoord;
+                        break;
+
+                    case "medicina":
+                        objektuSkipts.atraPalidziba.
+                              GetComponent<RectTransform>().localPosition =
+                              objektuSkipts.atrPKoord;
+                        break;
+
+                    case "buss":
+                        objektuSkipts.autobuss.
+                             GetComponent<RectTransform>().localPosition =
+                             objektuSkipts.bussKoord;
+                        break;
+
+				case "cement":
+					objektuSkipts.cementaMasina.
+					GetComponent<RectTransform>().localPosition =
+						objektuSkipts.cementKoord;
+					break;
+
+				case "e46":
+					objektuSkipts.e46.
+					GetComponent<RectTransform>().localPosition =
+						objektuSkipts.e46Koord;
+					break;
+
+				case "b2":
+					objektuSkipts.b2.
+					GetComponent<RectTransform>().localPosition =
+						objektuSkipts.b2Koord;
+					break;
+				case "policija":
+					objektuSkipts.policija.
+					GetComponent<RectTransform>().localPosition =
+						objektuSkipts.policijaKoord;
+					break;
+				case "eskavators":
+					objektuSkipts.eskavators.
+					GetComponent<RectTransform>().localPosition =
+						objektuSkipts.eskavatorsKoord;
+					break;
+				case "traktors1":
+					objektuSkipts.traktors1.
+					GetComponent<RectTransform>().localPosition =
+						objektuSkipts.traktors1Koord;
+					break;
+				case "traktors5":
+					objektuSkipts.traktors5.
+					GetComponent<RectTransform>().localPosition =
+						objektuSkipts.traktors5Koord;
+					break;
+				case "e61":
+					objektuSkipts.e61.
+					GetComponent<RectTransform>().localPosition =
+						objektuSkipts.e61Koord;
+					break;
+				case "ugunsdzeseji":
+					objektuSkipts.ugunsdzeseji.
+					GetComponent<RectTransform>().localPosition =
+						objektuSkipts.ugunsdzesejiKoord;
+					break;
+
+
+
+                    default:
+                        Debug.Log("Tags netika atpazīts!");
+                        break;
+                }
+            }
+		} 
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Skripti/Objekti.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objekti : MonoBehaviour {
+	//Uzglabās visus velkamos objektus
+	public GameObject atkritumuMasina;
+	public GameObject atraPalidziba;
+	public GameObject autobuss;
+	public GameObject cementaMasina;
+	public GameObject e46;
+	public GameObject b2;
+	public GameObject policija;
+	public GameObject eskavators;
+	public GameObject traktors1;
+	public GameObject traktors5;
+	public GameObject e61;
+	public GameObject ugunsdzeseji;
+
+
+	//Uzglabās katra transportlīdzekļa sākotnējo atrāšanās 
+	// vietu
+	[HideInInspector]
+	public Vector2 atkrMKoord;
+    [HideInInspector]
+    public Vector2 atrPKoord;
+    [HideInInspector]
+    public Vector2 bussKoord;
+	[HideInInspector]
+	public Vector2 cementKoord;
+	[HideInInspector]
+	public Vector2 e46Koord;
+	[HideInInspector]
+	public Vector2 b2Koord;
+	[HideInInspector]
+	public Vector2 policijaKoord;
+	[HideInInspector]
+	public Vector2 eskavatorsKoord;
+	[HideInInspector]
+	public Vector2 traktors1Koord;
+	[HideInInspector]
+	public Vector2 traktors5Koord;
+	[HideInInspector]
+	public Vector2 e61Koord;
+	[HideInInspector]
+	public Vector2 ugunsdzesejiKoord;
+
+	public Canvas kanva;
+
+	//Uzglabās audio avotu, kurā atskaņot 
+	// audio efektus
+	public AudioSource skanasAvots;
+	//Uzlgabās audio failus
+	public AudioClip[] skanaKoAtskanot;
+
+	[HideInInspector]
+	public bool vaiIstajaVieta = false;
+	public GameObject pedejaisVilktais = null;
+
+    void Start () {
+		//Uzsākot spēli piefiksē
+		// kur sākotnēji atrodas katra mašīna
+		atkrMKoord =
+		atkritumuMasina.GetComponent<RectTransform>().localPosition;
+        
+		atrPKoord =
+        atraPalidziba.GetComponent<RectTransform>().localPosition;
+        
+		bussKoord =
+        autobuss.GetComponent<RectTransform>().localPosition;
+		
+		cementKoord =
+			cementaMasina.GetComponent<RectTransform>().localPosition;
+		
+		e46Koord =
+			e46.GetComponent<RectTransform>().localPosition;
+		
+		b2Koord =
+			b2.GetComponent<RectTransform>().localPosition;
+		
+		policijaKoord =
+			policija.GetComponent<RectTransform>().localPosition;
+		
+		eskavatorsKoord =
+			eskavators.GetComponent<RectTransform>().localPosition;
+		
+		traktors1Koord =
+			traktors1.GetComponent<RectTransform>().localPosition;
+		
+		traktors5Koord =
+			traktors5.GetComponent<RectTransform>().localPosition;
+		
+		e61Koord =
+			e61.GetComponent<RectTransform>().localPosition;
+		
+		ugunsdzesejiKoord =
+			ugunsdzeseji.GetComponent<RectTransform>().localPosition;
+		
+    }
+}
